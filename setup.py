625451bbc5af372e8ecf4ad9efa08d4538b5d1d7
abhishek.joshi <abhishek.joshi@ihsmarkit.com>
2019-01-10 17:43:48 -0500

    upgraded version

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 setup(
     name = 'pyrainzplay',
     packages = find_packages(),
-    version = '0.0.4',
+    version = '0.0.5',
     license='MIT',
     description = 'Playground app for py first timers!',
     author = 'Rainz',
     author_email = 'cbludev@gmail.com',
     url = 'https://github.com/cbludev/pyrainzplay',
-    download_url = 'https://github.com/cbludev/pyrainzplay/archive/0.0.4.zip',
+    download_url = 'https://github.com/cbludev/pyrainzplay/archive/0.0.5.zip',
     keywords = ['numpy'],
     install_requires=[
         'numpy',
