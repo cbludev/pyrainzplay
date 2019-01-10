69fcb557793c26553c50027c76ed4bdaa9109b20
abhishek.joshi <abhishek.joshi@ihsmarkit.com>
2019-01-10 16:49:30 -0500

    upgrading download url version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Rainz',
     author_email = 'cbludev@gmail.com',
     url = 'https://github.com/cbludev/pyrainzplay',
-    download_url = 'https://github.com/cbludev/pyrainzplay/archive/pyrainzplay_0.0.1.tar.gz',
+    download_url = 'https://github.com/cbludev/pyrainzplay/archive/pyrainzplay_0.0.2.tar.gz',
     keywords = ['numpy'],
     install_requires=[
         'numpy',
