2bae07cdbe535b62ae4b24d4de07375605fb0d36
abhishek.joshi <abhishek.joshi@ihsmarkit.com>
2019-01-10 17:26:20 -0500

    more structure to it

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup
+from setuptools import find_packages
+
 setup(
     name = 'pyrainzplay',
-    packages = ['pyrainzplay'],
-    version = '0.0.3',
+    packages = find_packages(),
+    version = '0.0.4',
     license='MIT',
     description = 'Playground app for py first timers!',
     author = 'Rainz',
     author_email = 'cbludev@gmail.com',
     url = 'https://github.com/cbludev/pyrainzplay',
-    download_url = 'https://github.com/cbludev/pyrainzplay/archive/0.0.3.zip',
+    download_url = 'https://github.com/cbludev/pyrainzplay/archive/0.0.4.zip',
     keywords = ['numpy'],
     install_requires=[
         'numpy',
