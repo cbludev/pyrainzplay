e0bd0ad02861aa6de6665b5516930359a2bac5c2
abhishek.joshi <abhishek.joshi@ihsmarkit.com>
2019-01-10 16:51:19 -0500

    upgrading version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name = 'pyrainzplay',
     packages = ['pyrainzplay'],
-    version = '0.0.2',
+    version = '0.0.3',
     license='MIT',
     description = 'Playground app for py first timers!',
     author = 'Rainz',
     author_email = 'cbludev@gmail.com',
     url = 'https://github.com/cbludev/pyrainzplay',
-    download_url = 'https://github.com/cbludev/pyrainzplay/archive/pyrainzplay_0.0.2.tar.gz',
+    download_url = 'https://github.com/cbludev/pyrainzplay/archive/0.0.3.zip',
     keywords = ['numpy'],
     install_requires=[
         'numpy',
