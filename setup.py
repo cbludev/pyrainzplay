c03d49876ed83b123262032df927bf56671485ee
cbludev <42965056+cbludev@users.noreply.github.com>
2019-01-10 15:27:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,32 +7,10 @@ setup(
     description = 'Playground app for py first timers!',
     author = 'Rainz',
     author_email = 'cbludev@gmail.com',
-    url = 'https://github.com/joelbarmettlerUZH/Scrapeasy',
-    download_url = 'https://github.com/joelbarmettlerUZH/Scrapeasy/archive/pypi-0_1_3.tar.gz',
-    keywords = ['scraping', 'easy', 'scraper', 'website', 'download', 'links', 'images', 'videos'],
+    url = 'https://github.com/cbludev/pyrainzplay',
+    download_url = 'https://github.com/cbludev/pyrainzplay/archive/pyrainzplay_0.0.1.tar.gz',
+    keywords = ['numpy'],
     install_requires=[
-        'validators',
-        'beautifulsoup4',
-    ],
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'numpy',
     ],
 )
